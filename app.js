2e69a75c398415a4af5af7460f487555373355c3
lostsleepyboii <alexei77756@gmail.com>
2020-10-27 11:00:10 +0500

    Сделал навигацию по сайту, исправил некоторые баги

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
 $(function() {
 
+	/* Fixed Header */
 	let header = $("#header");
 	let intro = $("#intro");
 	let introH = intro.innerHeight();
 	let scrollPos = $(window).scrollTop();
 
+	checkScroll(scrollPos, introH);
+
 	$(window).on("scroll load resize", function() {
 		introH = intro.innerHeight();
 		scrollPos = $(this).scrollTop();
 
+		checkScroll(scrollPos, introH);
+	});
+
+	function checkScroll(scrollPos, introH) {
 		if (scrollPos > introH) {
 			header.addClass("fixed");
 		} else {
 			header.removeClass("fixed");
 		}
+	}
 
-		console.log ("scrollsPos =", scrollPos);
+	/* Smooth Scroll */
+	$("[data-scroll]").on("click", function(event) {
+		event.preventDefault();
 
+		let blockID = $(this).data('scroll');
+		let blockOffset = $(blockID).offset().top;
+
+		$("html, body").animate ({scrollTop: blockOffset - 100}, 700)
 	});
 
-	
+
+
  });
